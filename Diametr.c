6ac30f95b92e9aa547d256a920442e0289b0cf8b
Davit Mkrtumyan <141499493+dav777dav@users.noreply.github.com>
2025-07-11 23:39:47 +0400

    Diametr.c

--- Diametr.c
+++ Diametr.c
@@ -6,10 +6,10 @@ int main() {
     int a = 0;
     int b = 0;
     int c = 0;
-    printf("Grel yereq hat tiv: ");
+    printf("put three numbers: ");
     scanf("%d %d %d", &a, &b, &c);
-    printf("yndhanur: %d\n", a + b + c);
-    printf("mijin: %f", (a+b+c)/3);
+    printf("general: %d\n", a + b + c);
+    printf("average: %f", (a+b+c)/3);
     return 0;
 
 }
