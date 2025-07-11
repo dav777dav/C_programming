3f6bd6cb577c86009f4fd65750b4a281e9152af2
Davit Mkrtumyan <141499493+dav777dav@users.noreply.github.com>
2025-07-11 23:37:55 +0400

    A - a.c

--- A - a.c
+++ A - a.c
@@ -4,7 +4,7 @@ int main() {
 
     int num1 = 0;
     int num2 = 0;
-    printf("grel yerku tiv: ");
+    printf("put two numbers: ");
     scanf("%d %d", &num1, &num2);
     printf("%d\n", num1+num2);
     return 0;
