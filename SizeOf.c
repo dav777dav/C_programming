55587e9698a2e75d32c341acdd0cd19d6dee6c16
Davit Mkrtumyan <141499493+dav777dav@users.noreply.github.com>
2025-07-11 23:40:20 +0400

    SizeOf.c

--- SizeOf.c
+++ SizeOf.c
@@ -4,7 +4,7 @@ int main() {
 
     int num1 = 0;
     int num2 = 0;
-    printf("Grel erku tiv: ");
+    printf("put two numbers: ");
     scanf("%d %d", &num1, &num2);
     printf("%d", num1 % num2);
     return 0;
