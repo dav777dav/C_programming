4b59d2aac843708e8481977fb717ea98a7c2a9d2
Davit Mkrtumyan <141499493+dav777dav@users.noreply.github.com>
2025-07-24 20:48:18 +0400

    Create uppercase-lowercase.c

---
+++ if_else_loops/uppercase-lowercase.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("Enter an uppercase letter: ");
+
+    char sym = 0;
+  
+    scanf("%c", &sym);
+    
+    if(sym >= 'A' && sym <= 'Z') {
+        printf("Lowercase: %c\n", sym + 32);
+    } else {
+        printf("This is not an uppercase letter!\n");
+    }
+    return 0;
+}
