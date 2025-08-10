bfab586d65620364d09dcc7e6ce762a7b13d9154
Davit Mkrtumyan <141499493+dav777dav@users.noreply.github.com>
2025-08-10 22:06:35 +0400

    Create even_odd.cpp

---
+++ bitwise_operations/even_odd.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num = 0;
+  
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num & 1) {
+        printf("%d is odd.\n", num);
+    }
+    else {
+        printf("%d is even.\n", num);
+    }
+  
+    return 0;
+}
