b6ca794427d100d4111f43dc9906482393ef353c
Davit Mkrtumyan <141499493+dav777dav@users.noreply.github.com>
2025-08-10 12:41:14 +0400

    Create is_even.c

---
+++ if_else_loops/is_even.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 0;
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    if(num % 2 == 0) {
+        printf("%d is even number.\n", num);
+    }
+    else {
+        printf("%d is odd number.\n", num);
+    }
+
+    return 0;
+}
