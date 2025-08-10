03fa70e1094e400e3863390c0a2523709ed3f8ec
Davit Mkrtumyan <141499493+dav777dav@users.noreply.github.com>
2025-08-10 13:07:41 +0400

    Update is_even.c

--- if_else_loops/is_even.c
+++ if_else_loops/is_even.c
@@ -6,13 +6,22 @@ int main()
     
     printf("Enter a number: ");
     scanf("%d", &num);
+
+    int is_even = 0;
     
     if(num % 2 == 0) {
+        is_even = 1;
+    }
+    else {
+        is_even = 0;
+    }
+
+    if(is_even) {
         printf("%d is even number.\n", num);
     }
     else {
         printf("%d is odd number.\n", num);
     }
-
+    
     return 0;
 }
