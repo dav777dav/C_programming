93fca12e7a3e480d5d731c5fd3b27441e9793954
Davit Mkrtumyan <141499493+dav777dav@users.noreply.github.com>
2025-08-10 13:09:11 +0400

    Update is_cold.c

--- if_else_loops/is_cold.c
+++ if_else_loops/is_cold.c
@@ -6,8 +6,17 @@ int main()
     
     printf("Enter the temperature in Celsius: ");
     scanf("%d", &temp);
+
+    int is_cold = 0;
     
     if(temp < 15) {
+        is_cold = 1;
+    }
+    else {
+        is_cold = 0;
+    }
+
+    if(is_cold) {
         printf("It is cold weather.\n");
     }
     else {
