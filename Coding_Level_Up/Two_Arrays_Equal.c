f57213232c735e774529196d30fdec6385f8ef80
Davit Mkrtumyan <141499493+dav777dav@users.noreply.github.com>
2025-08-11 16:08:05 +0400

    Add files via upload

---
+++ Coding_Level_Up/Two_Arrays_Equal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int arraysEqual(int a[], int b[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (a[i] != b[i])
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int arr1[] = {1, 2, 3};
+    int arr2[] = {1, 2, 3};
+    int size = 3;
+
+    if (arraysEqual(arr1, arr2, size))
+        printf("Arrays are equal\n");
+    else
+        printf("Arrays are not equal\n");
+
+    return 0;
+}
